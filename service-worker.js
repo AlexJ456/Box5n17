a899d8662ab7d36017cc7920a6f0f60d20eda07c
AlexJ456 <alexjohanson22@gmail.com>
2026-01-21 07:27:08 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'breathing-exercises-cache-v7'; // Versioned cache name
+const CACHE_NAME = 'breathing-exercises-cache-v8'; // Versioned cache name
 const urlsToCache = [
   './',
   './index.html',
